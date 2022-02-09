3ba1f3d409d100c98e4dfe01052f8c1fe2b8d89d
guswnsakvk <guswnsakvk1@naver.com>
2022-02-09 14:21:50 +0900

    score

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const canvas = document.querySelector("#canvas")
 const ctx = canvas.getContext("2d")
+const score = document.querySelector(".score")
 
 canvas.width = window.innerWidth - 100
 canvas.height = window.innerHeight - 150
@@ -37,6 +38,7 @@ let animation
 function moveObstacle(){
   animation = requestAnimationFrame(moveObstacle)
   timer++
+  score.innerText = parseInt(timer / 75)
   ctx.clearRect(0,0, canvas.width, canvas.height)
 
   if(timer % 100 == 0){
